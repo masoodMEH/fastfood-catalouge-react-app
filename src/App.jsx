b6fc3de473b5d349fe10c0554c37c34cfa73f379
Your Name <you@example.com>
2024-08-07 15:58:25 +0330

    create header component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./pages/Header/Header";
+
+const App = () => {
+  return (
+    <div className="wrapper bg-faded-dark">
+      <Header></Header>
+    </div>
+  );
+};
+
+export default App;
