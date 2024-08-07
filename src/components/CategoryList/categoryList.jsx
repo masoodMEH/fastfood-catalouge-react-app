5b4334e5f234bc5589712cc54dd27fdd5fba172a
Your Name <you@example.com>
2024-08-07 16:37:30 +0330

    showing menu items in categoryList Header

--- src/components/CategoryList/categoryList.jsx
+++ src/components/CategoryList/categoryList.jsx
@@ -1,17 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "../../axios";
 
 const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get("/FoodCategory/categories");
-      console.log(response.data);
+      setCategories(response.data);
     };
 
     fetchCategories();
   }, []);
 
-  return <div>CategoryList</div>;
+  return (
+    <nav className="container mt-n5">
+      <div className="d-flex align-items-center bg-white rounded-3 shadow-xl py-4 h-[80px]">
+        <ul className="nav">
+          <li className="nav-item">
+            <a href="#" className="nav-link">
+              همه فست فود ها
+            </a>
+          </li>
+          {categories.map((category) => (
+            <li className="nav-item" key={category.id}>
+              <a href="#" className="nav-link">
+                {category.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default CategoryList;
