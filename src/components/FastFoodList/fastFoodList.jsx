98cc2427555d1d6f629e701ea6a666beea2174fd
Your Name <you@example.com>
2024-08-07 18:33:36 +0330

    items of FastFoodItem component implemented in main page

--- src/components/FastFoodList/fastFoodList.jsx
+++ src/components/FastFoodList/fastFoodList.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import FastFoodItem from "../FastFoodItem/fastFoodItem";
 
-const FastFoodList = () => {
-  return <div>Fast Food Items</div>;
+const FastFoodList = ({ fastFoodItems }) => {
+  return (
+    <div className="row">
+      {fastFoodItems.map((fastfood) => {
+        return (
+          <div className="col-md-4 col-sm-6 mb-grid-gutter" key={fastfood.id}>
+            <FastFoodItem {...fastfood} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FastFoodList;
